6df66dfa32d2d122223be1142db2d18b625fa0ef
Ngoc Do <ngocdo@maruweb.vn>
2023-06-22 23:36:42 +0700

    Fix some bugs

--- src/user/routes.rs
+++ src/user/routes.rs
@@ -1,5 +1,5 @@
 use crate::api_error::ApiError;
-use crate::user::{User, UserMessage};
+use crate::user::model::{User, UserMessage};
 use actix_web::{delete, get, post, put, web, HttpResponse};
 use serde_json::json;
 use uuid::Uuid;
